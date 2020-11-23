b635d0c368e8afe5df633289c09343b5fd442f1e
Smail <smaillounes1@gmail.com>
2020-11-23 22:13:19 +0100

    set first project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './Main/app.component';
+import { HomeComponent } from './Pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path:'', component: AppComponent, children: [
+    {path: 'home', component: HomeComponent}
+  ]}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './Main/app.component';
+import { HeaderComponent } from './Layouts/header/header.component';
+import { FooterComponent } from './Layouts/footer/footer.component';
+import { MenuComponent } from './Layouts/menu/menu.component';
+import { SidenavMenuComponent } from './Layouts/sidenav-menu/sidenav-menu.component';
+import { HomeComponent } from './Pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    SidenavMenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
